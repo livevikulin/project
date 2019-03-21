88bba3ca186037edb5308497e031bf6a3a1f046e
livevikulin <47454540+livevikulin@users.noreply.github.com>
2019-03-21 15:46:32 +0700

    Макет без адаптива

---
+++ js/slider-clients.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.clients-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: ('.a-right'),
+        nextArrow: ('.a-left'),
+    });
+});
---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.portfolio-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: ('.arrows-right'),
+        nextArrow: ('.arrows-left')
+    });
+});
